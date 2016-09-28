928774b30736c58c16d22b12acfd41838cb3ff8f
bytezen <rhazes.spell@gmail.com>
2016-09-28 14:52:58 -0700

    totem example and jQuery ui for colors animate

---
+++ storm_web/myTotem.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function () {
+    "use strict";
+    console.log("lets rock and roll.");
+    
+    
+});
+
+
+
+/*
+
+var $tigernose = $("#yedamkim-nose2")
+
+
+$tigernose.animate({
+    left: 50
+}).animate({ left: -50, queue: true }).animate({left: 0, queue:true})
+[<div id=​"yedamkim-nose2" style=​"position:​ relative;​ top:​ 0px;​ left:​ 0px;​">​</div>​]
+
+
+$tigernose.animate({borderBottomColor: "rgb(179,89,9)"},1000).animate({borderBottomColor: "yellow", queue: true}, 500)
+[<div id=​"yedamkim-nose2" style=​"border-bottom-color:​ rgb(255, 255, 0)​;​">​</div>​]
+
+
+
+function colorize() {
+   $tigernose.animate({borderBottomColor: "green"}, 500).animate({borderBottomColor: "yellow", queue: true},500,
+      function() {
+           count--;
+           if(count > 0 ) { colorize(); }
+        })
+}
+
+*/
