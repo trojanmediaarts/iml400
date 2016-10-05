4ee279075675db5b5fad032b18ca6bc6c597e5e3
bytezen <rhazes.spell@gmail.com>
2016-10-05 11:17:29 -0700

    demo working - unstyled

--- week7/code/spotify-example/js/main.js
+++ week7/code/spotify-example/js/main.js
@@ -2,6 +2,81 @@ var result = {};
 
 $(document).ready(function() {
     
+    /*
+        this function will 
+    */
+    function getResultItems(res,type) {
+        var data = {};
+        switch(type) {
+            case 'artist':
+                data.items = res.artists.items;
+                break;
+            case 'playlist':
+                data.items = res.playlists.items;
+                break;                
+        }
+        return data;
+    }
+    
+    function makePlaylistDisplayObject(anItem) {
+        var name = "unknown",            
+            img = "img/default.jpg",
+            owner = "unknown",
+            ownerUrl = "http://spotify.com",
+            total = 0;
+        
+        if( _.has(anItem, "name") ) {
+            name = anItem.name;
+        }
+            
+        if( _.has(anItem,"images") && anItem.images.length > 1 ) { 
+            img = anItem.images[1].url; 
+        }      
+        
+        if( _.has(anItem, "owner","id") ) {
+            owner = anItem.owner.id;
+        }
+        
+        if( _.has(anItem, "owner","external_urls","spotify") ) {
+            ownerUrl = anItem.owner.external_urls.spotify
+        }
+            
+        if( _.has(anItem, "tracks","total") ) {            
+            total = anItem.tracks.total;
+        }
+  
+        
+        //console.log(name,img,owner,ownerUrl,total);                 
+        return {"name": name, "img": img, "owner": owner, "ownerUrl": ownerUrl, "total": total};
+    }
+    
+//    function buildPlaylistObjectForDisplay(items) {
+//        var ret = { items: [] };
+//        
+//        _.each(items )
+//        item.name;
+//        if(item.images.length > 1) {
+//            item.images[1].url    
+//        } else 
+//            //no url -- or a dummy placeholder image    
+//        }
+//        item.owner.id,
+//        item.owner.external_urls.spotify
+//        item.tracks.total
+//        return ret;
+//    }
+    
+//    result = getResultItems(artistsData,'artist');
+    result = getResultItems(playlistsData,'playlist');
+    
+    var displayData = { items: _.map(result.items, makePlaylistDisplayObject) } 
+    
+    result = displayData;
+    w3DisplayData("spotifyResults", result);
+    
+//    w3DisplayData("spotifyResults", serverData);
+    
+    /*
     var baseURL = "http://api.spotify.com";
     var searchURL = baseURL + "/v1/search";
     
@@ -23,6 +98,7 @@ $(document).ready(function() {
         var params = {q: artist, type:'artist'};
         $.get(searchURL, params).done(onDataReceive).fail(onError)
     }
+    */
     /*
     $.get(searchURL+"?q='anderson paak'&type=artist").done( function(data) {
         document.writeln(" got data " );
@@ -38,5 +114,5 @@ $(document).ready(function() {
 //    
 //    })
     
-    searchArtist('anderson paak');
+//    searchArtist('anderson paak');
 })
